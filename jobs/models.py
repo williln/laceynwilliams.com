4384201eccefa5b2253fec76eb9c1551afa7432e
Lacey Williams Henschel <laceynwilliams@gmail.com>
2015-11-05 17:05:05 -0800

    Edited required fields

--- jobs/models.py
+++ jobs/models.py
@@ -5,15 +5,14 @@ from django.utils import timezone
 class Job(models.Model):
     title = models.CharField(max_length=200)
     company = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=2) 
     added_date = models.DateTimeField(default=timezone.now)
     start_date = models.DateTimeField(default=timezone.now)
-    #todo: add "present" to end date
     end_date = models.DateTimeField(default=timezone.now, blank=True)
     still_working = models.BooleanField()
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title + " (" + self.company + ")"
@@ -21,12 +20,12 @@ class Job(models.Model):
 class Talk(models.Model):
     title = models.CharField(max_length=200)
     conference = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     added_date = models.DateTimeField(default=timezone.now)
     talk_date = models.DateTimeField(default=timezone.now)
-    url = models.URLField(max_length=200)
-    slides = models.URLField(max_length=200)
-    video = models.URLField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
+    slides = models.URLField(max_length=200, blank=True)
+    video = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
@@ -34,10 +33,10 @@ class Talk(models.Model):
 class Publication(models.Model):
     title = models.CharField(max_length=200)
     publications = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     added_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(default=timezone.now)
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title   
@@ -48,7 +47,7 @@ class Education(models.Model):
     state = models.CharField(max_length=2) 
     grad_date = models.DateTimeField(default=timezone.now)
     degree = models.CharField(max_length=200) 
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.degree + ', ' + self.university
